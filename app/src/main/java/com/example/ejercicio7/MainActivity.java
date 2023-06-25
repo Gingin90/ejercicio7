a1a035fe5ec454a3c0f216dea5cb2068bf74740e
ginger cea <gingercea@gmail.com>
2023-06-24 22:56:59 -0400

    se ha creado los themes mas style , main se dio funcionalidad para mostrar un mensaje

--- app/src/main/java/com/example/ejercicio7/MainActivity.java
+++ app/src/main/java/com/example/ejercicio7/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ejercicio7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,15 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initListeners();
     }
 
+    private void initListeners() {
 
     TextView text =findViewById(R.id.textView);
     EditText name= findViewById(R.id.editTextName);
+    Button button = findViewById(R.id.floatingActionButton);
+
+
+    //EditText name.setOnClickListener(new View.OnClickListener(){
+
+    //Button floatingActionButton.setOnClickListener(new View.OnClickListener(){
+      //  @Override
+          //  public void onCLick(View v){
+        }//
+
 
     private void showToast(){
-        Toast.makeText(getBaseContext(), "Hello Toast",Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), "Bienvenido Toast",Toast.LENGTH_LONG).show();
 
 
     }
+
+
 }
