2b1f3a37d7ee7f26509cd8186c31ee20998ed0b2
ginger cea <gingercea@gmail.com>
2023-06-25 12:58:34 -0400

    se han realizado cambios en main activity para darle funcionalidad a los componentes

--- app/src/main/java/com/example/ejercicio7/MainActivity.java
+++ app/src/main/java/com/example/ejercicio7/MainActivity.java
@@ -59,32 +59,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-        //CalendarView.OnDateChangeListener(){
-           // onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth);
-
-          //  public abstract void onDateChanged (DatePicker view,
-            //int year, int monthOfYear, int dayOfMonth)//
-
+        Toast.makeText(getBaseContext(), "Bienvenido Toast", Toast.LENGTH_LONG).show();
         }
-       // Toast.makeText(getBaseContext(), "Bienvenido Toast", Toast.LENGTH_LONG).show();
+    });
         }
-    //});
 
 
 
-// Projection array. Creating indices for this array instead of doing
-// dynamic lookups improves performance.
-//public static final String[] EVENT_PROJECTION = new String[] {
-//        Calendars._ID,                           // 0
-//        Calendars.ACCOUNT_NAME,                  // 1
- //       Calendars.CALENDAR_DISPLAY_NAME,         // 2
-//        Calendars.OWNER_ACCOUNT                  // 3
- //       };)
+//CalendarView.OnDateChangeListener(){
+// onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth);
 
-// The indices for the projection array above.
-//private static final int PROJECTION_ID_INDEX = 0;
-//private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-//private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-//private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+//  public abstract void onDateChanged (DatePicker view,
+//int year, int monthOfYear, int dayOfMonth)//
+
