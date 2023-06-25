13a989ec2886badc1eb11c35fdf4466c878f9af1
ginger cea <gingercea@gmail.com>
2023-06-25 12:54:05 -0400

    se han realizado cambios en main activity para darle funcionalidad a los componentes

--- app/src/main/java/com/example/ejercicio7/MainActivity.java
+++ app/src/main/java/com/example/ejercicio7/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.ejercicio7;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,31 +23,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG, "onCreate: inicio de la creacion ");
 
         initListeners();
     }
 
     private void initListeners() {
 
-    TextView text =findViewById(R.id.textView);
-    EditText name= findViewById(R.id.editTextName);
-    Button button = findViewById(R.id.floatingActionButton);
+        TextView text = findViewById(R.id.textView);
+        EditText name = findViewById(R.id.editTextName);
+        FloatingActionButton flu = findViewById(R.id.floatingActionButton);
+        CalendarView calendar= findViewById(R.id.calendarView);
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
-    //EditText name.setOnClickListener(new View.OnClickListener(){
+        flu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
 
-    //Button floatingActionButton.setOnClickListener(new View.OnClickListener(){
-      //  @Override
-          //  public void onCLick(View v){
-        }//
-
-
-    private void showToast(){
-        Toast.makeText(getBaseContext(), "Bienvenido Toast",Toast.LENGTH_LONG).show();
+            }
+        });
 
 
-    }
+
+        //CalendarView.OnDateChangeListener(){
+           // onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth);
+
+          //  public abstract void onDateChanged (DatePicker view,
+            //int year, int monthOfYear, int dayOfMonth)//
+
+        }
+       // Toast.makeText(getBaseContext(), "Bienvenido Toast", Toast.LENGTH_LONG).show();
+        }
+    //});
 
 
-}
+
+// Projection array. Creating indices for this array instead of doing
+// dynamic lookups improves performance.
+//public static final String[] EVENT_PROJECTION = new String[] {
+//        Calendars._ID,                           // 0
+//        Calendars.ACCOUNT_NAME,                  // 1
+ //       Calendars.CALENDAR_DISPLAY_NAME,         // 2
+//        Calendars.OWNER_ACCOUNT                  // 3
+ //       };)
+
+// The indices for the projection array above.
+//private static final int PROJECTION_ID_INDEX = 0;
+//private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+//private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+//private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
